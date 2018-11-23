c757e72839ca13c6767da6676f98e14e78c84e3f
Alberto Aldana <albertoaldanar@hotmail.com>
2018-11-22 21:28:51 -0700

    V2 game card redy

--- src/components/top.js
+++ src/components/top.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View, Text, Image, FlatList, TouchableOpacity, RefreshControl} from "react-native";
+import {View, Text, Image, FlatList, TouchableOpacity, RefreshControl, ScrollView} from "react-native";
 import Games from "../constants/games";
 import FontAwesome, {Icons} from "react-native-fontawesome";
 
@@ -10,86 +10,92 @@ class Top extends Component{
     this.state = {refreshing: false}
   }
 
-  changeState(){
-    this.setState({refreshing: !this.state.refreshing})
+  showTopGames(){
+    return Games.map( (g) => {
+      return(
+        <TouchableOpacity>
+          <View style = {styles.card}>
+            <View style = {styles.desc}>
+              <Text style = {styles.league}>{g.league}</Text>
+                <Text style = {styles.hour}>{g.time}</Text>
+              </View>
+
+              <View style = {styles.match}>
+                <FontAwesome style= {[styles.chevron, {color: "gray", fontSize: 35}]}>{Icons.circle}</FontAwesome>
+
+                <View style = {{paddingRight: 110}}>
+                  <Text style = {styles.text}>{g.local}</Text>
+                  <Text style = {[styles.text, {fontSize: 13, fontStyle: "oblique", fontWeight: "400"}]}>VS.</Text>
+                  <Text style = {styles.text}>{g.visit}</Text>
+                </View>
+
+                <FontAwesome style= {styles.chevron}>{Icons.chevronRight}</FontAwesome>
+              </View>
+          </View>
+        </TouchableOpacity>
+      );
+    })
   }
+
   render(){
+    console.log(Games)
     return(
-      <View style = {{flex: 1, backgroundColor: "#1A1919"}}>
-        <FlatList
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.refreshing}
-              onRefresh={this.changeState.bind(this)}
-            />
-          }
-          style={styles.list}
-          data={Games}
-          renderItem={({item}) =>
-          <TouchableOpacity>
-            <View style = {styles.card}>
-                <View>
-                  <FontAwesome style ={{color: "white", fontSize: 20}}>{Icons.calendar}</FontAwesome>
-                </View>
-
-                <View style = {styles.teams}>
-                  <View>
-                    <Text style = {styles.textTeam}>{item.local}</Text>
-                    <View style = {styles.teamColors}></View>
-                  </View>
-
-                  <Text style = {styles.textVs}> VS </Text>
-
-                  <View>
-                    <Text style = {styles.textTeam}>{item.visit}</Text>
-                    <View style = {{paddingTop:4, backgroundColor: "red"}}></View>
-                  </View>
-                </View>
-
-                <View>
-                  <FontAwesome style ={{color: "white"}}>{Icons.chevronRight}</FontAwesome>
-                </View>
-
-            </View>
-          </TouchableOpacity>
-          }
-          keyExtractor={(item, index) => index}
-        />
+      <View style = {{flex: 1, backgroundColor: "#161616"}}>
+        <ScrollView>
+          {this.showTopGames()}
+        </ScrollView>
       </View>
     );
   }
 }
 
-
 const styles = {
   card: {
-    borderBottomColor: "gray",
-    borderBottomWidth: 0.5,
-    flexDirection: "row",
-    padding: 40,
-    justifyContent: "space-between"
+    padding: 13,
+    margin: 5,
+    backgroundColor: "#1A1919",
+    borderRadius: 3,
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    elevation: 0.3,
   },
-  teams: {
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
-  textTeam: {
+  text: {
     color: "white",
+    fontWeight: "700",
+    fontSize: 18,
+    paddingBottom: 5
+  },
+  league: {
+    color: "#4DE39E",
     fontWeight: "bold",
-    fontSize: 17,
-    paddingBottom: 3
+    fontSize: 14
   },
-  textVs:{
-    color: "white",
-    fontSize: 15,
-    paddingRight: 10,
-    paddingLeft: 10
+  match: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
-  teamColors: {
-    paddingTop: 4,
-    backgroundColor: "blue"
+  desc: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+     marginBottom: 12
+  },
+  hour: {
+    fontWeight: "300",
+    fontSize: 12,
+    color: "gray",
+    fontStyle: "oblique"
+  },
+  chevron: {
+    color: "#4DE39E",
+    fontSize: 20,
+    marginTop: 20,
+    fontWeight: "400"
   }
-
 }
 
 export default Top;
--- src/constants/games.js
+++ src/constants/games.js
@@ -4,26 +4,31 @@ const Games = [
     visit: "Liverpool",
     time: "5 FEB 9:00pm",
     league: "Premier League",
-    localColor: "#1E90FF",
-    visitColor: "#B22222"
   },
   {
     local: "Barcelona",
-    visit: "Real Madird",
+    visit: "Real Madrid",
     time: "5 FEB 9:00pm",
     league: "La liga",
-    localColor: "#1E90FF",
-    visitColor: "#B22222"
   },
   {
     local: "Valencia",
     visit: "Sevilla",
     time: "5 FEB 9:00pm",
     league: "La liga",
-    localColor: "#1E90FF",
-    visitColor: "#B22222"
+  },
+  {
+    local: "AC Milan",
+    visit: "Inter Milan",
+    time: "5 FEB 9:00pm",
+    league: "Serie A",
+  },
+  {
+    local: "Boca Juniors",
+    visit: "River Plate",
+    time: "5 FEB 9:00pm",
+    league: "Copa Libertadores",
   }
 ]
-
 
 module.exports = Games;

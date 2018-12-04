4fd745246b07a463130b44e12ed02df20f6ee1d8
Alberto Aldana <albertoaldanar@hotmail.com>
2018-12-03 17:54:13 -0700

    Image slider working fine but needs some desgign changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View, Text, Image, TouchableOpacity, Modal, Dimensions} from "react-native";
+import {View, Text, Image, TouchableOpacity, Modal, Dimensions, StatusBar, ScrollView} from "react-native";
 import LinearGradient from "react-native-linear-gradient"
 import FontAwesome, {Icons} from "react-native-fontawesome";
 import Leagues from "./leagues";
@@ -7,7 +7,6 @@ import BMButton from "../reusable/bmButton"
 import Header from "../reusable/header";
 import ImageSlider from 'react-native-image-slider';
 
-var tennis = require ('../images/tennis.png');
 
 class Home extends Component{
 
@@ -22,31 +21,41 @@ class Home extends Component{
 
   render(){
 
-
     const images = [
-        'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bbb5a131251589.56488ec072087.png',
-        'https://i.imgur.com/31G5r5w.jpg?1',
-        "https://ep01.epimg.net/deportes/imagenes/2017/03/10/actualidad/1489164710_500738_1489165158_noticia_normal.jpg"
-    ];
+      'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bbb5a131251589.56488ec072087.png',
+      'https://i.imgur.com/31G5r5w.jpg?1',
+      "https://ep01.epimg.net/deportes/imagenes/2017/03/10/actualidad/1489164710_500738_1489165158_noticia_normal.jpg"
+    ]
+
+    const header = ["BET . MATCH . WIN", "Bet against other people", "The new way to bet"];
+
+    const msg = ["Bet in the best leagues in the world", "Bet agains randoms and your friends in the world in your favorite sport events", "Bet from anywhere against anyone"]
 
     return(
       <View style = {{flex: 1, backgroundColor: "#1A1919"}}>
         <Header/>
+        <ScrollView>
+        <StatusBar hidden = {true}/>
+
 
         <View style = {styles.images}>
           <ImageSlider
-              style = {styles.images}
               images= {images}
               autoPlayWithInterval={2500}
-          />
-        </View>
+              customSlide={({ index, item, style, width }) => (
+                <View key={index}>
+                  <Image source = {{uri: item}} style = {style}/>
 
-        <View style = {{margin: 10, marginTop: -220}}>
-          <Text style = {styles.title}> Bet agains other people</Text>
-          <Text style = {styles.secondText}> Bet agains your friends and randoms around the world in your favorite sport events </Text>
-          <TouchableOpacity style = {styles.button}>
-            <Text style = {styles.buttonText}> BET NOW </Text>
-          </TouchableOpacity>
+                  <View style = {styles.messageContainer}>
+                    <Text style = {styles.title}> {header[index]} </Text>
+                    <Text style = {styles.secondText}> {msg[index]} </Text>
+                    <TouchableOpacity style = {styles.button}>
+                      <Text style = {styles.buttonText}>BET NOW</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+          />
         </View>
 
         <Modal
@@ -58,6 +67,7 @@ class Home extends Component{
               closeModal = {this.onModalChange.bind(this)}
             />
         </Modal>
+        </ScrollView>
       </View>
     );
   }
@@ -68,32 +78,39 @@ const styles = {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height * 0.35,
     alignSelf: "center",
-    borderRadius: 5
+    borderRadius: 5,
   },
   title: {
     color: "#ffff",
     fontWeight: "bold",
-    fontSize: 25,
-    justifyContent: "flex-start",
-    marginBottom: 13
+    fontSize: 17,
+    margin: 10,
+    marginBottom: 30
   },
   secondText: {
     color: "#ffff",
     fontWeight: "500",
-    fontSize: 18,
-    justifyContent: "flex-start",
-    marginBottom: 20
+    fontSize: 15,
+    margin: 10,
   },
   button: {
     backgroundColor: "#00B073",
     padding: 8,
     borderRadius: 5,
-    width: Dimensions.get("window").width * 0.3
+    width: Dimensions.get("window").width * 0.3,
+    margin: 10
   },
   buttonText: {
     color: "#ffff",
     fontSize: 17,
-    alignSelf: "center"
+    alignSelf: "center",
+  },
+  messageContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    position: "absolute",
+    marginTop: 10
   }
 }
 

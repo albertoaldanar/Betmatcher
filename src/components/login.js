3a3aae2e8ffebc3bf3b55c688150e411009e0f03
Alberto Aldana <albertoaldanar@hotmail.com>
2019-05-28 22:15:52 -0600

    Signup and json web token are working correctley

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {View, Text, TextInput, TouchableOpacity, Image, Dimensions} from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import axios from "axios";
+import deviceStorage from '../services/deviceStorage';
 
 class Login extends Component{
 
@@ -17,24 +18,31 @@ class Login extends Component{
   }
 
   userSignup(){
-    const {username, password} = this.state;
+    const {username, password, password_confirmation, email} = this.state;
 
-     axios.post("http://localhost:8000/users/signup",{
-      user: {
-        email: email,
-        password: password,
-        password_confirmation: password_confirmation
-      }
-    },)
-    .then((response) => {
-       // Handle the JWT response here
-    })
-    .catch((error) => {
+      return fetch("http://localhost:8000/users/signup/", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({"username": username, "email": email, "password": password, "password_confirmation": password_confirmation})
+      })
+      .then(res => res.json())
+      .then(jsonRes => {
+        console.log(jsonRes)
+      })
+      .catch(error => console.log(error));
+  }
 
+  onChangeInput = (state) => (event,value) => {
+    this.setState({
+      [state]:event
     });
   }
 
   registrationForm(){
+    const {email, password_confirmation, password, username} = this.state;
     if(this.state.isSignup){
       return(
         <View style = {styles.inputs}>
@@ -43,6 +51,8 @@ class Login extends Component{
             placeholder = "Username"
             placeholderTextColor = "gray"
             autoCapitalize = 'none'
+            onChangeText ={this.onChangeInput('username')}
+            value = {username}
           />
 
           <TextInput
@@ -50,6 +60,8 @@ class Login extends Component{
             placeholder = "Email"
             placeholderTextColor = "gray"
             autoCapitalize = 'none'
+            onChangeText ={this.onChangeInput('email')}
+            value = {email}
           />
           <TextInput
             style={{height: 40, borderBottomColor: 'white', borderBottomWidth: 0.5, color:"white", color: "white", marginBottom: 25 }}
@@ -57,6 +69,8 @@ class Login extends Component{
             placeholderTextColor = "gray"
             secureTextEntry={true}
             autoCapitalize = 'none'
+            onChangeText ={this.onChangeInput('password')}
+            value = {password}
           />
           <TextInput
             style={{height: 40, borderBottomColor: 'white', borderBottomWidth: 0.5, color:"white", color: "white"}}
@@ -64,17 +78,19 @@ class Login extends Component{
             placeholderTextColor = "gray"
             secureTextEntry={true}
             autoCapitalize = 'none'
+            onChangeText ={this.onChangeInput('password_confirmation')}
+            value = {password_confirmation}
           />
 
           <View style = {{marginLeft: 15, marginRight: 15, marginTop: 55}}>
-            <TouchableOpacity style = {{backgroundColor: "#00B073", paddingTop: 10, paddingBottom: 10}}>
+            <TouchableOpacity style = {{backgroundColor: "#00B073", paddingTop: 10, paddingBottom: 10}} onPress= {this.userSignup.bind(this)}>
               <Text style = {{textAlign: "center", color: "white", fontWeight: "300", fontSize: 16}}>Sign up</Text>
             </TouchableOpacity>
           </View>
 
           <View style = {{ position: "absolute", bottom: 15, flexDirection: "row", alignSelf:"center"}}>
             <Text style= {{fontWeight: "300", color: "#DCDCDC"}}>Do yoy have an acount? </Text>
-            <TouchableOpacity onPress= {() => this.setState({isSignup: false})}>
+            <TouchableOpacity onPress= {() => this.setState({isSignup: false, username: "", password: ""})}>
               <Text style= {{fontWeight: "600", color: "#00B073"}}> Sign in </Text>
             </TouchableOpacity>
           </View>
@@ -109,7 +125,7 @@ class Login extends Component{
 
           <View style = {{ position: "absolute", bottom: 15, flexDirection: "row", alignSelf:"center"}}>
             <Text style= {{fontWeight: "300", color: "#DCDCDC"}}>Dont have an acount? </Text>
-            <TouchableOpacity onPress= {() => this.setState({isSignup: true})}>
+            <TouchableOpacity onPress= {() => this.setState({isSignup: true, username: "", password: ""})}>
               <Text style= {{fontWeight: "600", color: "#00B073"}}> Sign up </Text>
             </TouchableOpacity>
           </View>
@@ -119,7 +135,7 @@ class Login extends Component{
   }
 
   render(){
-    console.log(this.state.isSignup);
+    console.log(this.state.username, this.state.email);
 
     return(
       <LinearGradient style = {{flex: 1}} start={{x: 1, y: 1}} end={{x: 4 , y: 0}} colors = {[ "#161616", "gray"]}>

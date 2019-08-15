414e1e652bfa1d7b86c3ff2bbc5368d0e4818e47
Alberto Aldana Ríos <albertoaldanarios@MacBook-Pro-de-Alberto.local>
2019-08-15 12:22:51 -0600

    Changes in profile design

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,15 +7,17 @@ import FontAwesome, {Icons} from "react-native-fontawesome";
 import LinearGradient from "react-native-linear-gradient";
 import ProfileCharts from "../reusable/profileCharts";
 import {
-  LineChart,
   BarChart,
   ProgressChart,
-  PieChart,
   ContributionGraph,
   StackedBarChart
 } from 'react-native-chart-kit'
 import {Pages} from "react-native-pages";
 import Url from "../constants/url";
+import MaterialTabs from "react-native-material-tabs";
+import PieChart from 'react-native-pie-chart';
+import { LineChart, YAxis, Grid } from 'react-native-svg-charts';
+
 
 class Profile extends Component{
 
@@ -24,7 +26,7 @@ class Profile extends Component{
 
   constructor(props){
     super(props);
-    this.state = {username:"", won: "", lost:"", draw:"", country: "", currentUser: "", currentToken: "", coins: 0}
+    this.state = {username:"", won: "", lost:"", draw:"", country: "", currentUser: "", currentToken: "", coins: 0, index: 0}
   }
 
   componentWillUnmount() {
@@ -88,6 +90,26 @@ class Profile extends Component{
       .catch(error => console.log(error));
   }
 
+  changeIndex(index){
+    this.setState({ index });
+    this.getUserProfits(this);
+  }
+
+  getUserProfits(){
+    return fetch(`http://${Url}:8000/user_records?current_user=${this.state.currentUser}`, {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-type": "application/json",
+        }
+      })
+      .then(res => res.json())
+      .then(jsonRes => {
+        console.log(jsonRes)
+      })
+      .catch(error => console.log(error));
+  }
+
   renderFriends(){
     const navigateAction = NavigationActions.navigate({
       routeName: "Friends",
@@ -98,14 +120,108 @@ class Profile extends Component{
     this.props.navigation.dispatch(navigateAction);
   }
 
+  choseView(){
+    const {index} = this.state;
+
+    const chart_wh = 200
+    const series = [123, 321, 123,]
+    const sliceColor = ['#00B073','#2196F3','red']
+    switch(index){
+      case 0:
+        return(
+            <View style = {{justifyContent:"space-around", marginBottom: 15, marginTop: 45,height: 200, width: 200,}}>
+
+                <View style = {{justifyContent: "space-around", flexDirection:"row"}}>
+                    <View>
+                      <Text style = {[styles.username, {fontSize: 14, fontWeight:"300", color: "gray", marginBottom: 5}]}>
+                        Coins
+                      </Text>
+                      <Text style = {[styles.username, {fontSize: 16, fontWeight:"300", color: "#DAA520", marginBottom: 7, marginTop: 3}]}>
+                        {this.state.coins}  <FontAwesome style = {{color: "#DAA520"}}>{Icons.database}</FontAwesome>
+                      </Text>
+                    </View>
+
+                    <View>
+                      <Text style = {[styles.username, {fontSize: 14, fontWeight:"300", color: "gray", marginBottom: 5}]}>
+                        Efficiency
+                      </Text>
+                      <Text style = {[styles.username, {fontSize: 16, fontWeight:"300", color: "#DAA520", marginBottom: 7, marginTop: 3}]}>
+                        67.7 %
+                      </Text>
+                    </View>
+
+                    <View>
+                      <Text style = {[styles.username, {fontSize: 14, fontWeight:"300", color: "gray", marginBottom: 5}]}>
+                        Efficiency
+                      </Text>
+                      <Text style = {[styles.username, {fontSize: 16, fontWeight:"300", color: "#DAA520", marginBottom: 7, marginTop: 3}]}>
+                        67.7 %
+                      </Text>
+                    </View>
+                </View>
+            </View>
+            
+          );  
+
+        break;
+
+      case 1:
+        return(
+            <ScrollView style = {{marginTop: 10}}>
+                <View style = {{flexDirection: "row", justifyContent: "space-between", marginTop: 20, borderBottomWidth: 0.3, borderBottomColor: "gray", paddingBottom: 15, marginLeft: 20, marginRight: 20}}>
+                  <View style = {{marginLeft: 25}}>
+                    <Text style = {{color:"white", fontSize: 13}}>Arsenal</Text>
+                    <Text style = {{color:"white", fontSize: 13}}>vs</Text>
+                    <Text style = {{color:"white", fontSize: 13}}>Southhampton</Text>
+                  </View>
+
+                  <View style = {{ marginRight: 25, marginTop: 9,}}>
+                    <Text style = {{color:"#00B073" , alignSelf: "center", marginBottom: 3}}>WON <FontAwesome>{Icons.sortUp}</FontAwesome></Text>
+                    <Text style = {{color:"gray", alignSelf:"center"}}> + 560 <FontAwesome>{Icons.database}</FontAwesome></Text>
+                  </View>
+                </View>
+
+                <View style = {{flexDirection: "row", justifyContent: "space-between", marginTop: 20, borderBottomWidth: 0.3, borderBottomColor: "gray", paddingBottom: 15, marginLeft: 20, marginRight: 20}}>
+                  <View style = {{marginLeft: 25}}>
+                    <Text style = {{color:"white", fontSize: 13}}>Monterrey</Text>
+                    <Text style = {{color:"white", fontSize: 13}}>vs</Text>
+                    <Text style = {{color:"white", fontSize: 13}}>Tigres</Text>
+                  </View>
+ 
+
+                  <View style = {{ marginRight: 25, marginTop: 9,}}>
+                    <Text style = {{color:"red" , alignSelf: "center", marginBottom: 3}}>LOSS <FontAwesome>{Icons.sortDown}</FontAwesome></Text>
+                    <Text style = {{color:"gray", alignSelf:"center"}}> - 560 <FontAwesome>{Icons.database}</FontAwesome></Text>
+                  </View>
+                </View>
+
+                <View style = {{flexDirection: "row", justifyContent: "space-between", marginTop: 20, borderBottomWidth: 0.3, borderBottomColor: "gray", paddingBottom: 15, marginLeft: 20, marginRight: 20}}>
+                  <View style = {{marginLeft: 25}}>
+                    <Text style = {{color:"white", fontSize: 13}}>Monterrey</Text>
+                    <Text style = {{color:"white", fontSize: 13}}>vs</Text>
+                    <Text style = {{color:"white", fontSize: 13}}>Tigres</Text>
+                  </View>
+
+                  <View style = {{ marginRight: 25, marginTop: 9,}}>
+                    <Text style = {{color:"#00B073" , alignSelf: "center", marginBottom: 3}}>WON <FontAwesome>{Icons.sortUp}</FontAwesome></Text>
+                    <Text style = {{color:"gray", alignSelf:"center"}}> + 560 <FontAwesome>{Icons.database}</FontAwesome></Text>
+                  </View>
+                </View>
+            </ScrollView>
+        );
+        break;
+    }
+  }
+
   render(){
     const {won, lost, draw, country, username, coins} = this.state;
     console.log(this.state.currentUser);
+    console.log(lost, draw, won)
 
     const data = [
-      { name: 'Won', number: 3, color: "rgba(0,176,115,0.2)", legendFontColor: '#7F7F7F', legendFontSize: 15 },
-      { name: 'Draw', number: 1, color: '#DCDCDC', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-      { name: 'Lost', number: 1, color: '#DC143C', legendFontColor: '#7F7F7F', legendFontSize: 15 }
+      { name: 'Won', number: Number(won), color: "rgba(0,176,115,0.2)", legendFontColor: '#7F7F7F', legendFontSize: 15 },
+      { name: 'Draw', number: Number(draw), color: '#DCDCDC', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+      { name: 'Lost', number: Number(lost), color: '#DC143C', legendFontColor: '#7F7F7F', legendFontSize: 15 }
     ];
 
     const data2 = [0.4, 0.6, 0.8]
@@ -124,7 +240,7 @@ class Profile extends Component{
     }
 
     return(
-      <View style = {{flex: 1, backgroundColor: "black"}}>
+      <View style = {{flex: 1, backgroundColor: "#161616"}}>
 
         <StatusBar hidden = {true}/>
           <View style = {{backgroundColor: "#00B073"}}>
@@ -172,112 +288,20 @@ class Profile extends Component{
           </LinearGradient>
         </View>
 
-            <View style = {{justifyContent:"space-between", flexDirection:"row", marginBottom: 18, marginTop:20}}>
-                <View style = {{marginLeft: 16}}>
-                  <Text style = {[styles.username, {fontSize: 13, fontWeight:"300", color: "gray", marginBottom: 5}]}>
-                    Coins
-                  </Text>
-                  <Text style = {[styles.username, {fontSize: 14, fontWeight:"300", color: "#DAA520", marginBottom: 7, marginTop: 3}]}>
-                    {coins}  <FontAwesome style = {{color: "#DAA520"}}>{Icons.database}</FontAwesome>
-                  </Text>
-                </View>
-
-                <View style = {{marginRight: 16}}>
-                  <Text style = {[styles.username, {fontSize: 13, fontWeight:"300", color: "gray", marginBottom: 5}]}>
-                    World ranking
-                  </Text>
-                  <Text style = {[styles.username, {fontSize: 14, fontWeight:"300", color: "#DAA520", marginBottom: 7, marginTop: 3}]}>
-                    12892 º
-                  </Text>
-                </View>
-
-
-                <View style = {{marginRight: 16}}>
-                  <Text style = {[styles.username, {fontSize: 13, fontWeight:"300", color: "gray", marginBottom: 5}]}>
-                    Efficiency
-                  </Text>
-                  <Text style = {[styles.username, {fontSize: 14, fontWeight:"300", color: "#DAA520", marginBottom: 7, marginTop: 3}]}>
-                    67.7 %
-                  </Text>
-                </View>
-            </View>
-
-          <Pages>
-              <LineChart
-              data={{
-                labels: [],
-                datasets: [{
-                  data: [
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                  ]
-                }]
-              }}
-              width={Dimensions.get('window').width}
-              height={250}
-              chartConfig={{
-                backgroundColor: '#161616',
-                decimalPlaces: 0, // optional, defaults to 2dp
-                 color: (opacity = 0.5) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                  borderRadius: 16
-                }
-              }}
-              bezier
-              style={{
-                marginVertical: 8,
-                borderRadius: 16
-              }}
+          <View style = {{marginTop: 10}}>
+            <MaterialTabs
+                items={["Stats", "Profits & Losts"]}
+                indicatorColor ="gray"
+                activeTextColor ="white"
+                textStyle= {{fontSize: 12.5}}
+                inactiveTextColor ="gray"
+                barColor ="transparent"
+                selectedIndex={this.state.index}
+                onChange={this.changeIndex.bind(this)}
             />
+          </View>
 
-            <PieChart
-              data={data}
-              width={Dimensions.get("window").width}
-              height={220}
-              chartConfig={{
-                backgroundColor: 'transparent',
-                decimalPlaces: 0, // optional, defaults to 2dp
-                color: (opacity = 0.3) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                  borderRadius: 16
-                }
-              }}
-              style={{
-                marginVertical: 8,
-                borderRadius: 16,
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 6,
-                },
-                shadowOpacity: 0.37,
-                shadowRadius: 7.49,
-
-                elevation: 12,
-              }}
-              accessor="number"
-              backgroundColor="transparent"
-              paddingLeft="15"
-            />
-
-          </Pages>
+          {this.choseView()}
       </View>
     );
   }
